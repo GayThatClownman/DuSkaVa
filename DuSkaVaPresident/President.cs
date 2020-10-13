694ab824c174fee0d79627ad08f6e6e5ef543920
GayThatClownman <60339604+GayThatClownman@users.noreply.github.com>
2020-10-13 13:38:19 +0200

    Klass klar

---
+++ DuSkaVaPresident/President.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace DuSkaVaPresident
+{
+    public class President
+    {
+        public string name;
+        private int environmentPriority;
+        private int defensePriority;
+        private int healthcarePriority;
+        private int corruption;
+        private int money;
+
+        public President(string n)
+        {
+
+        }
+
+        public void Present()
+        {
+
+        }
+
+        // public bool PassLegislation(int e, int d, int h, int bribe)
+        // {
+        //     return ;
+        // }
+    }
+}
